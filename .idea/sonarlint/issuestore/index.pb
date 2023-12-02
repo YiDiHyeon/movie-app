3786992e28e9c9c031720d97947124235518fe3c
이지현 <leedmswl123@gmail.com>
2023-12-02 21:18:04 +0900

    영화 연도별 검색 select 수정

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -12,15 +12,7 @@ K
 src/components/MovieItem.ts,7/c/7c298f361de9ef99b4600b6208660a9defb52920
 ?
 src/custom.d.ts,4/7/4765e0a31eea1eead84e1b0fa95dbd2426c9ec4b
-4
-.env,3/c/3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
-<
-api/movie.ts,6/2/628bb153fc9b715404cd24177cdd740e05d14119
 O
 src/components/MovieListMore.ts,c/1/c17f79e88be10fdabd10a82aae435eefb7652e2d
-K
-src/components/TheHeader.ts,1/9/1927e530c5c05074e14744fadd18d8c6349df098
-B
-src/core/heropy.ts,2/2/22cbc259f5a38d42bf8e8eb7e2257326239db21a
 R
 ".parcel-cache/40ddeda4a7a72709.txt,c/1/c1064863e07e0efaf155c3010f8b4101074d558e
--- api/movie.ts
+++ api/movie.ts
@@ -3,10 +3,10 @@ import  { VercelRequest, VercelResponse} from "@vercel/node"
 
 const { APIKEY } = process.env
 export default async function handler(request : VercelRequest, response : VercelResponse) {
-    const { title, page, id } = JSON.parse(request.body)
+    const { title, page, year, id } = JSON.parse(request.body)
     const url = id
         ? `https://omdbapi.com?apikey=${APIKEY}&i=${id}&plot=full`
-        : `https://omdbapi.com?apikey=${APIKEY}&s=${title}&page=${page}`
+        : `https://omdbapi.com?apikey=${APIKEY}&s=${title}&page=${page}&y=${year}`
 
     const res = await fetch(url)
     const json = await res.json()
